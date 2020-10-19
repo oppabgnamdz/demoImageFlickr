c780540d2849deba649fe3b45273eae9795880ce
nam <namdeptrai@nams-MacBook-Pro.local>
2020-10-19 17:03:06 +0700

    complete download

---
+++ Category.js
@@ -0,0 +1,131 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, FlatList, SafeAreaView, ScrollView, TouchableHighlight } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
+
+const Category = ({ navigation }) => {
+    const [pictures, setPictures] = useState([]);
+    const [pictures2, setPictures2] = useState([]);
+    const _pressImage = () => {
+        navigation.navigate('ImageComponent')
+    }
+    useEffect(() => {
+        fetch('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=8fd2ff49a182d79766bf850bef1d9501&tags=cats&format=json&nojsoncallback=1&api_sig=dd5701757ba3b93b812c47886d2bf78f')
+            .then((respond) =>
+                respond.json()
+            ).then((result) => {
+                let dataImage = result.photos.photo.map((pic, index) => {
+                    let srcPath = "https://farm" + pic.farm + ".staticflickr.com/" + pic.server + "/" + pic.id + "_" + pic.secret + ".jpg"
+                    if (index < 50) {
+                        return (
+                            <TouchableHighlight
+                                key={pic.id}
+
+                                onPress={_pressImage}
+                                style={styles.touch}
+                            >
+                                <View>
+                                    <Image
+                                        resizeMode='contain'
+                                        source={{ uri: srcPath }}
+                                        style={{ width: 200, height: (Math.random() * 30) + 140, borderRadius: 10 }}
+                                    />
+                                    <View
+                                        style={styles.view}
+                                    >
+                                        <Entypo name="eye" size={24} color="black" />
+                                        <Text>{Math.ceil(Math.random() * 300)} View</Text>
+                                    </View>
+                                </View>
+                            </TouchableHighlight>
+                        )
+                    }
+                })
+                let dataImage2 = result.photos.photo.map((pic, index) => {
+                    let srcPath = "https://farm" + pic.farm + ".staticflickr.com/" + pic.server + "/" + pic.id + "_" + pic.secret + ".jpg"
+
+                    if (index >= 50) {
+                        return (
+                            <TouchableHighlight
+                                key={pic.id}
+
+                                onPress={_pressImage}
+                                style={styles.touch}
+                            >
+                                <View>
+                                    <Image
+                                        resizeMode='contain'
+                                        source={{ uri: srcPath }}
+                                        style={{ width: 200, height: Math.random() * 30 + 120, borderRadius: 10 }}
+                                    />
+                                    <View
+                                        style={styles.view}
+                                    >
+                                        <Entypo name="eye" size={24} color="black" />
+                                        <Text>{Math.ceil(Math.random() * 300)} View</Text>
+                                    </View>
+                                </View>
+                            </TouchableHighlight>
+                        )
+                    }
+                })
+                setPictures(dataImage)
+                setPictures2(dataImage2)
+            })
+    }, [])
+
+    return (
+        <SafeAreaView style={styles.container}>
+
+
+
+            <ScrollView>
+
+                <View style={{ flex: 1, justifyContent: 'space-around', flexDirection: 'row' }}>
+                    <View
+                        style={{ borderRadius: 20 }}
+                    >
+                        {pictures}
+                    </View>
+                    <View
+                        style={{ borderRadius: 20 }}
+                    >
+                        {pictures2}
+                    </View>
+                </View>
+
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+export default Category
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+    },
+    touch: {
+        marginTop: 10,
+        borderRadius: 40,
+    },
+    view: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        color: 'black',
+        backgroundColor: 'white',
+        opacity: 0.4,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
+
+    }
+});
---
+++ ImageComponent.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, FlatList, SafeAreaView, Dimensions, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
+import * as Permissions from 'expo-permissions';
+import { FontAwesome5 } from '@expo/vector-icons';
+import * as MediaLibrary from 'expo-media-library';
+import * as FileSystem from 'expo-file-system'
+import { set } from 'react-native-reanimated';
+const widthScreen = Dimensions.get('window').width;
+const ImageComponent = () => {
+    const [pictures, setPictures] = useState([]);
+    const [page, setPage] = useState(0);
+    useEffect(() => {
+        fetch('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=8fd2ff49a182d79766bf850bef1d9501&tags=dogs&page=12&format=json&nojsoncallback=1&api_sig=0e053aa7e296bf1f5e3ba6953d5637e7')
+            .then((respond) =>
+                respond.json()
+            ).then((result) => {
+                let fakeData = []
+                result.photos.photo.map((pic, index) => {
+                    let srcPath = "https://farm" + pic.farm + ".staticflickr.com/" + pic.server + "/" + pic.id + "_" + pic.secret + ".jpg"
+                    fakeData.push({
+                        index: index,
+                        id: pic.id,
+                        path: srcPath
+                    })
+                })
+                setPictures(fakeData)
+            })
+    }, [])
+    const renderItem = ({ item }) => (
+        <View>
+            <Image
+                resizeMode='contain'
+                source={{ uri: item.path }}
+                style={{ width: widthScreen, height: 300, borderRadius: 10 }}
+            />
+
+        </View>
+    );
+    const saveFile = async (fileUri) => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if (status === "granted") {
+            const asset = await MediaLibrary.createAssetAsync(fileUri)
+            await MediaLibrary.createAlbumAsync("Download", asset)
+            alert("Lưu ảnh thành công")
+        }
+    }
+    const downloadFile = () => {
+        console.log(page)
+
+        let link2 = ''
+        pictures.map((item) => {
+            if (item.index === page) {
+                link2 = item.path;
+            }
+        })
+        console.log(link2)
+        let fileUri = FileSystem.documentDirectory + "small.jpg";
+        FileSystem.downloadAsync(link2, fileUri)
+            .then(({ uri }) => {
+                saveFile(uri);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                // onScrollEndDrag={(item) => {
+                //     console.log(Math.floor(item.nativeEvent.contentOffset.x / item.nativeEvent.layoutMeasurement.width))
+
+                // }}
+                onMomentumScrollEnd={(item) => {
+                    const currentPage = Math.floor(item.nativeEvent.contentOffset.x / item.nativeEvent.layoutMeasurement.width);
+                    setPage(currentPage)
+                    console.log(currentPage)
+                }}
+                showsHorizontalScrollIndicator={true}
+                style={styles.flatlist}
+                pagingEnabled={true}
+                horizontal={true}
+                data={pictures}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
+            <TouchableOpacity
+                onPress={downloadFile}
+                style={{
+                    backgroundColor: 'cyan',
+                    width: 75, height: 75,
+                    borderRadius: 100,
+                    justifyContent: 'center', alignItems: 'center',
+                    position: 'absolute',
+                    right: 30,
+                    bottom: 100
+                }}
+            >
+                <FontAwesome5 name="download" size={24} color="black" />
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
+}
+
+export default ImageComponent
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black'
+    },
+    flatlist: {
+        marginTop: 200
+    }
+})
